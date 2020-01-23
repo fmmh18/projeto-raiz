71acee241252c6732f1bd83529bcf0285533afff
HayashidaFlavio <fmmh18@gmail.com>
2020-01-23 09:11:16 -0400

    #1 Initial Commit
    criação do projeto raiz com alguns respositórios

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+    require_once "vendor/autoload.php";
+    include_once "src/dependecies.php";
+    include_once "src/routes.php";
---
+++ src/dependecies.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => getenv('DB_DRIVER'),
+    'host'      => getenv('DB_HOST'),
+    'database'  => getenv('DB_DATABASE'),
+    'username'  => getenv('DB_USER'),
+    'password'  => getenv('DB_PASS'),
+    'charset'   => getenv('DB_CHARSET'),
+    'collation' => getenv('DB_COLLATION'),
+    'prefix'    => getenv('DB_PREFIX'),
+]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
+
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
